caf147d9461ce9a4546a4e1397836431050098a8
ichikawacraice <g.craice@gmail.com>
2022-01-25 11:07:46 -0300

    cineflix

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-undef
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
